73ed8762c1786a137eeae2903c0de69cd566887c
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-07-29 08:23:39 -0300

    Fix bug in the crawler

--- ecommerce_scrapper/ecommerce_scrapper/spiders/product_scrapper.py
+++ ecommerce_scrapper/ecommerce_scrapper/spiders/product_scrapper.py
@@ -6,20 +6,12 @@ class ProductSpider(scrapy.Spider):
     start_url = 'https://www.kabum.com.br/hardware/processadores'
 
     def start_requests(self):
-        urls = [
-            'https://www.kabum.com.br/produto/112990/processador-intel-core-i5-10400-cache-12mb-2-9ghz-4-3ghz-max-turbo-lga-1200-bx8070110400',
-            'https://www.kabum.com.br/produto/356937/placa-de-video-msi-nvidia-geforce-rtx-3070-gaming-z-trio-8g-8gb-gddr6-lhr-rgb-dlss-ray-tracing-geforce-rtx-3070-gaming-z-trio-8g-lhr',
-            'https://www.kabum.com.br/produto/81132/headset-gamer-hyperx-cloud-stinger-drivers-50mm-multiplas-plataformas-p2-e-p3-hx-hscs-bk-na',
-            'https://www.kabum.com.br/produto/358110/console-sony-playstation-5-horizon-forbidden-west',
-            'https://www.kabum.com.br/produto/115801/controle-sem-fio-ps5-dualsense'            
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=self.start_url, callback=self.parse)
+            
 
     def parse(self, response):
-        products = response.css('main a::attr(href)').getall()[:5]
+        products = response.xpath('//*[@id="listing"]/article/section/div[3]/div/main/div/a//@href').getall()
         for product in products:
-            print(product)
             yield scrapy.Request(url='https://www.kabum.com.br/' + product, callback=self.parse_detail)
 
     def parse_detail(self, response):
